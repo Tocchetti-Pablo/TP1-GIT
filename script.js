09a14444e78c0c802b89686472d43894b0d4287b
pablo_tocchetti <pablo_tocchetti@hotmail.com.ar>
2025-04-01 11:54:24 -0300

    Cambio en el archivo script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("Hola, soy Juan Chavez en la rama feature_prueba");
 console.log("Hola, soy Mario ricotti en la rama feature_prueba");
+console.log("Hola, soy Pablo Tocchetti en la rama feature_prueba");
